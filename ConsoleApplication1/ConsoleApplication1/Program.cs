603927f1e33e8aa459ac676e4ac4243e11a1d747
kadyvre <mitch04@live.com>
2016-10-04 10:21:22 -0400

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myText = "678";
+            int myInt;
+            bool isInt;
+
+            isInt = int.TryParse(myText, out myInt);
+
+            Console.WriteLine($"Integer: \"{myInt}\"");
+            Console.WriteLine($"Is Integer {isInt}");
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
